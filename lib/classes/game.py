c768703909668f92f33fe01e4c253a12591a8414
Abby <abby.e.smith.85@gmail.com>
2023-03-18 19:09:14 -0600

    passed 4 tests

--- lib/classes/game.py
+++ lib/classes/game.py
@@ -1,5 +1,15 @@
 from classes.result import Result
 
 class Game:
-    def __init__(self):
-        pass
+    def __init__(self, title):
+        self.title = title
+        
+    def get_title(self):
+        return self._title
+    
+    def set_title(self, title):
+        if (not hasattr(self, "_title")) and isinstance(title, str) and len(title) > 0:
+            self._title = title
+
+    title = property(get_title, set_title)
+
